e6af726f9fbfcb001a8c29e38e8bb97909edbea5
RohitG28 <rohit.ghivdonde@gmail.com>
2018-09-15 18:03:54 +0530

    Added comments for Epoch Extraction

--- readData.py
+++ readData.py
@@ -4,23 +4,34 @@ import numpy as np
 
 data = sio.loadmat('S01T.mat')
 
+## Following lists store the signal, trials, y and fs values for each subject separately
 signals = []
 trials = []
 y = []
 fs = []
 
 for i in range(len(data['data'][0])):
+	## transpose done to make channels as rows and time units as columns
 	signals.append(np.transpose(data['data'][0][i][0][0][0]))
 	trials.append(data['data'][0][i][0][0][1][0])
 	trials[i] = np.append(trials[i],len(signals[i][0]))
 	y.append(data['data'][0][i][0][0][2][0])
 	fs.append(data['data'][0][i][0][0][3][0])
 
+## number of channels in the data
 numChannels = len(signals[0])
+
+## epochStart = portion to be left after the trial start
 epochStart = 300
+
+## epochEnd = portion to be left before the trial end
 epochEnd = 0
+
+## length of an epoch in ms
 epochLen = 300
 
+
+## extracts epochs for each subject
 def epochExtractorPerSubject(signals, trials, y, epochs, epochClasses, epochLen, epochStart, epochEnd):
 	for i in range(len(trials)-1):
 		startTrial = trials[i]
